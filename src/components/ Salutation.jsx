9aad24ded7b001dbd98afde30c4951a419ccee62
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-29 15:09:33 -0300

    class component

---
+++ src/components/ Salutation.jsx
@@ -0,0 +1,36 @@
+import React, { Component} from 'react'
+
+export default class Salutation extends Component {
+  state = {
+    type: this.props.type,
+    name: this.props.name
+  }
+
+  setType(e) {
+    this.setState({ type: e.target.value })
+  }
+
+  setName(e) {
+    this.setState({ name: e.target.value })
+  }
+
+  render() {
+    const { type, name } = this.state
+    return (
+      <div>
+        <h1>{type}, {name}!</h1>
+        <hr />
+        <input 
+          type="text" 
+          placeholder="Type..." 
+          value={type} 
+          onChange={e => this.setType(e)}/>
+        <input 
+          type="text" 
+          placeholder="Name..." 
+          value={name} 
+          onChange={e => this.setName(e)}/>
+      </div>
+    )
+  }
+}
