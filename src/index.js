7ac1c49837abdf2571f16088a64910537247a3cf
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-29 15:09:48 -0300

    class component reference

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // import GoodMorning from "./components/GoodMorning";
-import { GoodAfternoon, GoodEvening } from "./components/ Multiples";
+// import { GoodAfternoon, GoodEvening } from "./components/ Multiples";
+
+import Salutation from "./components/ Salutation";
 
 ReactDOM.render(
   <div>
-    <GoodAfternoon name="Polyane" />
-    <GoodEvening name="Polyane" />
+    <Salutation type="Good morning" name="Poly" />
   </div>,
   document.getElementById("root")
 );
